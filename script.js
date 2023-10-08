ea1c8490eaeb43993df9b532b1b9aac7f1c0e3ce
xzyl4303 <115838895+xzyl4303@users.noreply.github.com>
2023-10-08 13:15:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,75 @@
-let currentPlayer = 'cross';
-let board = ['', '', '', '', '', '', '', '', ''];
-
-const cells = document.querySelectorAll('.cell');
-const message = document.getElementById('message');
-const overlay = document.getElementById('overlay');
-const popup = document.getElementById('popup');
-const resultMessage = document.getElementById('result');
-
-cells.forEach((cell, index) => {
-    cell.addEventListener('click', () => handleMove(index));
-});
-
-function handleMove(cell) {
-    if (board[cell] === '' && !checkWinner()) {
-        board[cell] = currentPlayer;
-        cells[cell].classList.add(currentPlayer);
-        if (checkWinner()) {
-            showOverlay(`玩家 ${currentPlayer === 'cross' ? '1 (❌)' : '2 (⭕️)'} 获胜！`);
-        } else if (!board.includes('')) {
-            showOverlay('平局！');
-        } else {
-            currentPlayer = currentPlayer === 'cross' ? 'circle' : 'cross';
-            message.innerText = `玩家 ${currentPlayer === 'cross' ? '1 (❌)' : '2 (⭕️)'} 的回合`;
-        }
-    }
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    margin: 0;
+    font-family: 'Arial', sans-serif;
+    background-color: #f5f5f5;
 }
 
-function checkWinner() {
-    const winPatterns = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
-
-    for (const pattern of winPatterns) {
-        const [a, b, c] = pattern;
-        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-            return true;
-        }
-    }
-
-    return false;
+.game-container {
+    text-align: center;
 }
 
-function showOverlay(result) {
-    resultMessage.innerText = result;
-    overlay.style.display = 'flex';
+.player-names {
+    margin-bottom: 20px;
 }
 
-function restartGame() {
-    currentPlayer = 'cross';
-    board = ['', '', '', '', '', '', '', '', ''];
-    cells.forEach(cell => {
-        cell.classList.remove('cross', 'circle');
-    });
-    overlay.style.display = 'none';
-    message.innerText = '玩家 1 的回合';
+.board {
+    display: grid;
+    grid-template-columns: repeat(3, 100px);
+    grid-template-rows: repeat(3, 100px);
+    grid-gap: 10px;
+    margin-bottom: 20px;
+}
+
+.cell {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 48px;
+    width: 100px;
+    height: 100px;
+    background-color: #eee;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+}
+
+.cell:hover {
+    background-color: #ddd;
+}
+
+.cross {
+    color: red;
+}
+
+.circle {
+    color: green;
+}
+
+.overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    display: none;
+}
+
+.popup {
+    background-color: white;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+}
+
+.popup button {
+    font-size: 16px;
+    cursor: pointer;
+    margin-top: 10px;
 }
